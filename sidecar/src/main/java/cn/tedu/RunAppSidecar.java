ff5463fcadc942e28e0b525c7e7b9367451f6ee0
Zmtomcat <min527992297@163.com>
2018-11-09 14:46:52 +0800

    jttest

---
+++ sidecar/src/main/java/cn/tedu/RunAppSidecar.java
@@ -0,0 +1,13 @@
+package cn.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+@SpringBootApplication
+@EnableSidecar
+public class RunAppSidecar {
+	public static void main(String[] args) {
+		SpringApplication.run(RunAppSidecar.class, args);
+	}
+}
